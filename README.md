08ee072b17e3727c74a1cbf517d76f3397b54ee9
ChangxingJiang <1278729001@qq.com>
2020-06-08 21:41:38 +0800

    新增：Bilibili直播弹幕爬虫(bilibili.barrage_of_live)

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 # CxSpider
 长行编写的爬虫的合集。包括如下爬虫：
 
-| 网站     | 爬虫                         | 爬虫路径               |
-| -------- | ---------------------------- | ---------------------- |
-| 微博     | 微博热搜榜实时爬虫           | weibo.hot_ranking      |
-| Twitter  | Twitter用户信息爬虫          | twitter.user_info      |
-| Twitter  | Twitter用户推文爬虫          | twitter.user_tweet     |
-| WanPlus  | 英雄联盟每日比赛列表爬虫     | wanplus.lol_date_list  |
-| WanPlus  | 英雄联盟比赛包含场次列表爬虫 | wanplus.lol_match_list |
-| WanPlus  | 英雄联盟场次详细信息爬虫     | wanplus.lol_match_info |
-| 中国知网 | 中国知网期刊包含刊期列表爬虫 | cnki.issue_list        |
-| 中国知网 | 英雄联盟比赛包含场次列表爬虫 | cnki.article_list      |
-| 虎牙     | 虎牙直播弹幕爬虫             | huya.barrage_of_live   |
-| 斗鱼     | 斗鱼直播弹幕爬虫             | douyu.barrage_of_live  |
+| 网站              | 爬虫                         | 爬虫路径                 |
+| ----------------- | ---------------------------- | ------------------------ |
+| 微博              | 微博热搜榜实时爬虫           | weibo.hot_ranking        |
+| Twitter           | Twitter用户信息爬虫          | twitter.user_info        |
+| Twitter           | Twitter用户推文爬虫          | twitter.user_tweet       |
+| WanPlus(玩加电竞) | 英雄联盟每日比赛列表爬虫     | wanplus.lol_date_list    |
+| WanPlus(玩加电竞) | 英雄联盟比赛包含场次列表爬虫 | wanplus.lol_match_list   |
+| WanPlus(玩加电竞) | 英雄联盟场次详细信息爬虫     | wanplus.lol_match_info   |
+| 中国知网          | 中国知网期刊包含刊期列表爬虫 | cnki.issue_list          |
+| 中国知网          | 英雄联盟比赛包含场次列表爬虫 | cnki.article_list        |
+| 虎牙              | 虎牙直播弹幕爬虫             | huya.barrage_of_live     |
+| 斗鱼              | 斗鱼直播弹幕爬虫             | douyu.barrage_of_live    |
+| Bilibili(B站)     | Bilibili直播弹幕爬虫         | bilibili.barrage_of_live |
 
 本合集中所有爬虫仅可用于学习、研究用途，不允许用于任何商业用途。如使将本合集中的任意爬虫用于商业用途，后果自负。
 
@@ -161,7 +162,30 @@
 | content    | 弹幕内容                                               |
 | text       | 弹幕其他信息                                           |
 
-### 7. WanPlus英雄联盟每日比赛列表爬虫(wanplus.lol_date_list)
+### 7. Bilibili直播弹幕爬虫(bilibili.barrage_of_live)
+
+> @author: ChangXing
+>
+> @version: 1.2
+>
+> @create: 2019.11.24
+>
+> @revise: 2020.06.08
+
+使用Selenium模拟浏览器，采集Bilibili直播间中的弹幕。
+
+* 应用配置：无需使用代理IP，需要使用Selenium
+
+| 字段名     | 字段内容                                               |
+| ---------- | ------------------------------------------------------ |
+| bid        | 弹幕ID                                                 |
+| type       | 弹幕类型                                               |
+| fetch_time | 弹幕采集时间（因实时采集，因此也可以视为弹幕发布时间） |
+| user_name  | 弹幕发布者名称                                         |
+| user_id    | 弹幕发布者ID                                           |
+| content    | 弹幕内容                                               |
+
+### 8. WanPlus英雄联盟每日比赛列表爬虫(wanplus.lol_date_list)
 
 > **@author** ChangXing
 >
@@ -190,7 +214,7 @@
 | team_a_score_per | LOL比赛的第1个参赛队伍各个小场的得分 |
 | team_b_score_per | LOL比赛的第2个参赛队伍各个小场的得分 |
 
-### 8. WanPlus英雄联盟比赛包含场次列表爬虫(wanplus.lol_match_list)
+### 9. WanPlus英雄联盟比赛包含场次列表爬虫(wanplus.lol_match_list)
 
 > **@author** ChangXing
 >
@@ -205,7 +229,7 @@
 * 目标Url：https://www.wanplus.com/schedule/58822.html
 * 应用配置：无需使用代理IP、无需使用Selenium
 
-### 9. WanPlus英雄联盟场次详细信息爬虫(wanplus.lol_match_info)
+### 10. WanPlus英雄联盟场次详细信息爬虫(wanplus.lol_match_info)
 
 > **@author** ChangXing
 >
@@ -220,7 +244,7 @@
 * 目标Url(实际请求的Ajax)：https://www.wanplus.com/ajax/matchdetail/65029?_gtk=345357323
 * 应用配置：无需使用代理IP、无需使用Selenium
 
-### 10. 微博热搜榜实时爬虫(weibo.hot_ranking)
+### 11. 微博热搜榜实时爬虫(weibo.hot_ranking)
 
 > **@author** ChangXing
 >
---
+++ bilibili/barrage_of_live.py
@@ -0,0 +1,105 @@
+# coding=utf-8
+
+"""
+Bilibili弹幕爬虫
+
+@author: ChangXing
+@version: 1.2
+@create: 2019.11.24
+@revise: 2020.06.08
+
+功能: 抓取直播间中所有弹幕，依据不同类型的弹幕提取不同信息，并存储于文件中
+说明: 使用Selenium打开Bilibili直播间，通过浏览器不断将Js渲染后的网页源代码传给BeautifulSoup解析弹幕
+目标Url: https://www.bilibili.com/topic/s9lol?rid=288016
+"""
+
+import time
+
+from bs4 import BeautifulSoup
+
+import toolkit as tool
+
+
+def crawler(live_name, live_url, mysql):
+    browser = tool.open_chrome(use_user_dir=False)  # 打开Chrome浏览器
+    browser.get(live_url)  # 访问目标Bilibili主播的直播间
+    time.sleep(10)
+
+    time_string = time.strftime("%Y%m%d_%H%M", time.localtime(time.time()))
+
+    table_name = "bilibili_{}".format(time_string)
+    sql_create = "CREATE TABLE live_barrage.`bilibili_{}` (" \
+                 "`bid` int(11) NOT NULL AUTO_INCREMENT COMMENT '弹幕ID(barrage id)'," \
+                 "`type` varchar(60) DEFAULT NULL COMMENT '弹幕类型'," \
+                 "`fetch_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '弹幕抓取时间(约等于弹幕发布时间)'," \
+                 " `user_name` varchar(40) DEFAULT NULL COMMENT '弹幕发布者名称'," \
+                 " `user_id` int(11) DEFAULT NULL COMMENT '弹幕发布者等级'," \
+                 " `content` varchar(100) DEFAULT NULL COMMENT '弹幕内容'," \
+                 " PRIMARY KEY (`bid`)" \
+                 ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Bilibili弹幕({})';"
+    mysql.create(sql_create.format(time_string, live_name))
+
+    print("开始抓取Bilibili直播弹幕.....")
+
+    total_time = 0
+    total_num = 0
+
+    update_time = 0
+
+    data_id_max = 0
+    for num in range(int(36000 / 0.5)):
+
+        start_time = time.time()
+
+        label_html = browser.find_element_by_id("chat-history-list").get_attribute("outerHTML")
+
+        soup = BeautifulSoup(label_html, 'lxml')  # 将网页内容解析为Soup对象
+
+        barrage_list = []
+        for label in soup.select("#chat-history-list > div"):
+
+            barrage_info = {
+                "type": "",  # 弹幕所属类型
+                "user_name": "",  # 弹幕发布者名称
+                "user_id": 0,  # 弹幕发布者等级
+                "content": "",  # 弹幕内容
+            }
+
+            type_class = label["class"]
+
+            if "danmaku-item" in type_class:
+                barrage_info["type"] = "NORMAL"
+            elif "welcome-msg" in type_class:
+                barrage_info["type"] = "ENTER"
+            elif "system-msg" in type_class:
+                barrage_info["type"] = "MESSAGE"
+            elif "gift-item" in type_class:
+                barrage_info["type"] = "GIFT"
+
+            if barrage_info.get("type") == "NORMAL":
+                temp_time = int(label["data-ts"])
+                if temp_time >= update_time:
+                    update_time = temp_time
+                    barrage_info["user_name"] = label["data-uname"]
+                    barrage_info["user_id"] = label["data-uid"]
+                    barrage_info["content"] = label["data-danmaku"]
+                    barrage_list.append(barrage_info)
+
+        mysql.insert_pure(table_name, barrage_list)
+
+        total_num += 1
+        total_time += 1000 * (time.time() - start_time)
+
+        wait_time = 0.5
+        if wait_time > (time.time() - start_time):
+            time.sleep(0.5 - (time.time() - start_time))
+
+        data_id_max += len(barrage_list)
+
+        print("本次时间范围内新增弹幕:", len(barrage_list), "条,", "(共计:", data_id_max, ")", "|",
+              "运行时间:", round(total_time / total_num), "毫秒", "(", round(total_time), "/", total_num, ")")
+
+
+if __name__ == "__main__":
+    crawler("20191110_LOL世界赛决赛(FPX vs G2)", "https://live.bilibili.com/blanc/6?liteVersion=true",
+            tool.mysql_connect("Barrage"))
