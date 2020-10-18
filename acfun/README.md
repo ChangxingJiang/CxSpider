2df4a3f3ce27ad2d22799b4616b42f33a604eafd
ChangxingJiang <1278729001@qq.com>
2020-10-18 14:29:50 +0800

    更新：AcFun视频信息爬虫（包括下载地址）

---
+++ acfun/README.md
@@ -0,0 +1,24 @@
+# AcFun视频信息爬虫（包括下载地址）
+
+> 最新有效性检查时间：2020.10.18
+
+#### 参数
+
+* page_url：视频页面地址
+
+#### 返回数据结果
+
+```json
+{
+	'视频标题': '这床真香！【紫颜】',
+	'视频下载地址': 'http://tx-safety-video.acfun.cn/mediacloud/acfun/acfun_video/BEI7FLXo9B-3APsUS5me1SI_ARzJ3xSHKGF_swFZ03UGhHN3W3-W6K6GvzH1Yyps.mp4?pkey=AAI6StIzW0mvB760uxP4-6xrKNC3v6zdcbL5NmpdnWPHVkX0k_MyclVvcWNGsjj4QZD9MThVr241kjtyB9KFjkCk3quzb7uc8KZ8MtNEr5SxbHiC3-VtdkUUkyKnFkH6jPJB_mWSRaUqbRhrgiDpa7w1jRgfteT1eneYnwHxaIVUzrAIvzbTtPwFVPf2b0do0DYkdRsveJvxckfamTHEZ3AAf0t-jZt9vY1rDlTSD8uwx9v-pg0Zo88rw5qSmxCFOSh4mLtJytBf0C7ABqWdYCqS_gx2PAE1Y9DsMsAdPTn3fQ'
+}
+```
+
+#### 调用Demo
+
+```python
+spider = Spider()
+print(spider.run(page_url="https://www.acfun.cn/v/ac16986343"))
+```
+
--- acfun/video.py
+++ acfun/video.py
@@ -1,34 +1,60 @@
 """
-AcFun视频信息爬虫
+AcFun视频信息爬虫（包括下载地址）
 
-@author: ChangXing
-@version: 1.0
-@create: 2020.07.24
-@revise: -
+@Author: 长行
+@Update: 2020.10.18
 """
 
 import re
 
+import Utils4R as Utils
 import requests
 
 
-def crawler(url: str):
-    headers = {
-        "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
-    }
-    info_url = "https://api-new.acfunchina.com/rest/app/play/playInfo/mp4?videoId={}&resourceId={}&resourceType=2&mkey=AAHewK3eIAAyMjAzNTI2NDMAAhAAMEP1uwS3Vi7NYAAAAJumF4MyTTFh5HGoyjW6ZpdjKymALUy9jZbsMTBVx-F10EhxyvpMtGQbBCYipvkMShM3iMNwbMd9DM6r2rnOYRVEdr6MaJS4yxxlA_Sl3JNWup57qBCQzOSC7SZnbEsHTQ%3D%3D&market=xiaomi&product=ACFUN_APP&sys_version=10&app_version=6.20.0.915&boardPlatform=sdm845&sys_name=android&socName=UNKNOWN&appMode=0"
+class Spider(Utils.abc.SingleSpider):
 
-    # 获取视频基本信息
-    response = requests.get(url, headers=headers).text
-    title = re.search(r"(?<=<title>)[^<]+(?=</title>)", response).group()  # 视频标题
-    video_id = re.search(r"(?<=\"vid\":\")\d+(?=\",)", response).group()  # 视频ID
-    resource_id = re.search(r"(?<=\"ac\":\")\d+(?=\",)", response).group()  # 视频资源ID
+    def __init__(self):
+        # 执行请求的请求头
+        self.headers = {
+            "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
+        }
 
-    # 获取视频Url
-    rep_info = requests.get(info_url.format(video_id, resource_id), headers=headers)
-    video_url = rep_info.json()["playInfo"]["streams"][0]["playUrls"][0]
-    return {"title": title, "videos": video_url}
+        # 视频信息的Url
+        self.download_url = ("https://api-new.acfunchina.com/rest/app/play/playInfo/mp4"
+                             "?videoId={}"
+                             "&resourceId={}"
+                             "&resourceType=2"
+                             "&mkey=AAHewK3eIAAyMjAzNTI2NDMAAhAAMEP1uwS3Vi7NYAAAAJumF4MyTTFh5HGoyjW6ZpdjKymALUy9jZbsMTBVx-F10EhxyvpMtGQbBCYipvkMShM3iMNwbMd9DM6r2rnOYRVEdr6MaJS4yxxlA_Sl3JNWup57qBCQzOSC7SZnbEsHTQ%3D%3D"
+                             "&market=xiaomi"
+                             "&product=ACFUN_APP"
+                             "&sys_version=10"
+                             "&app_version=6.20.0.915"
+                             "&boardPlatform=sdm845"
+                             "&sys_name=android"
+                             "&socName=UNKNOWN"
+                             "&appMode=0")
+
+    def run(self, **params):
+        if "page_url" not in params:
+            return False
+
+        # 获取视频基本信息
+        response = requests.get(params["page_url"], headers=self.headers).text
+        title = re.search(r"(?<=<title>)[^<]+(?=</title>)", response).group()  # 提取视频标题
+        video_id = re.search(r"(?<=\"vid\":\")\d+(?=\",)", response).group()  # 提取视频ID
+        resource_id = re.search(r"(?<=\"ac\":\")\d+(?=\",)", response).group()  # 提取视频资源ID
+
+        # 获取视频下载Url
+        rep_info = requests.get(self.download_url.format(video_id, resource_id), headers=self.headers)
+        video_url = rep_info.json()["playInfo"]["streams"][0]["playUrls"][0]
+
+        # 返回结果
+        return {
+            "视频标题": title,
+            "视频下载地址": video_url
+        }
 
 
 if __name__ == "__main__":
-    print(crawler("https://www.acfun.cn/v/ac16986343"))
+    spider = Spider()
+    print(spider.run(page_url="https://www.acfun.cn/v/ac16986343"))
