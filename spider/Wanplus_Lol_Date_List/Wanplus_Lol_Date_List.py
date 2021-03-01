b51afbc833a59201b003e78bf4e333444dbfc1ce
ChangxingJiang <1278729001@qq.com>
2021-03-01 14:52:48 +0800

    更新LOL比赛数据爬虫

--- spider/Wanplus_Lol_Date_List/Wanplus_Lol_Date_List.py
+++ spider/Wanplus_Lol_Date_List/Wanplus_Lol_Date_List.py
@@ -1,6 +1,7 @@
-import datetime
 import json
 import time
+from datetime import datetime
+from datetime import timedelta
 from typing import List, Dict
 
 import crawlertool as tool
@@ -29,76 +30,92 @@ class SpiderWanplusLolDateList(tool.abc.SingleSpider):
         "x-csrf-token": "345357323",
         "x-requested-with": "XMLHttpRequest"
     }
+
+    # 列表请求的表单数据
     _DATE_LIST_DATA = {
         "_gtk": "345357323",
         "game": "2",
         "time": "1571500800",
         "eids": "",
-    }  # 列表请求的表单数据
+    }
 
     def running(self, start_date, end_date) -> List[Dict]:
+        # 初始化返回结果数据
         result = []
 
-        # 统计需要抓取的日期列表
-        all_date_list = list()  # 需要获取的日期列表
+        # ----- 计算所有需要抓取的日期列表 -----
+        all_date_list = []  # 需要获取的日期列表
         curr_date = end_date
         while curr_date >= start_date:
             all_date_list.append(curr_date.strftime("%Y%m%d"))
-            curr_date += datetime.timedelta(days=-1)
-        # print("需要抓取的日期总数:", len(all_date_list), all_date_list)
+            curr_date += timedelta(days=-1)
 
-        if len(all_date_list) == 0:  # 若没有需要抓取的日期则结束运行
+        # 若没有需要抓取的日期则完成采集
+        if len(all_date_list) == 0:
             return result
 
-        # 统计需要追溯的日期所在的周的时间戳
-        need_date_list = list()  # 需要抓取的时间戳列表(所有的周日+最早的一天)
-        for curr_date_str in all_date_list:
-            curr_date = datetime.datetime.strptime(curr_date_str, "%Y%m%d")
-            if curr_date.weekday() == 0:  # 若时间戳为周日
-                need_date_list.append(curr_date_str)
-        need_date_list.append(all_date_list[-1])  # 添加最早的一天
-        # print("需要抓取的时间戳总数:", len(need_date_list))
+        # ----- 计算实际需要请求的日期列表 -----
+        # 每一次请求均会返回该日所在周7天的数据，因此只需要请求每周的周日即可，和最后一个还没有到周日的周即可
+        need_date_list = []
+        # 如果最后一周还没有到周日，则添加最后一周的最后一天
+        if datetime.strptime(all_date_list[0], "%Y%m%d").weekday() != 0:
+            need_date_list.append(all_date_list[0])
+        # 添加之前每一周的周日
+        for curr_date in all_date_list:
+            if datetime.strptime(curr_date, "%Y%m%d").weekday() == 0:
+                need_date_list.append(curr_date)
 
-        # 格式化日期，方便比较
-        end_date = end_date.strftime("%Y%m%d")
-        start_date = start_date.strftime("%Y%m%d")
-
-        # 依据时间戳抓取比赛数据
+        # ----- 依据时间戳抓取比赛数据 -----
         for i in range(len(need_date_list)):
-            curr_date_str = need_date_list[i]
-            # print("正在抓取时间戳:", i + 1, "/", len(need_date_list), "(", curr_date_str, ")")
-            curr_date_timestamp = str((datetime.datetime.strptime(curr_date_str, "%Y%m%d") - datetime.datetime(1970, 1, 1)).total_seconds())
-            self._DATE_LIST_DATA["time"] = curr_date_timestamp  # 列表请求的表单数据
-            response = tool.do_request(self._DATE_LIST_URL, method="post", headers=self._DATE_LIST_HEADERS, data=self._DATE_LIST_DATA)
-            # print(response.content)
+
+            print("当前抓取:", self.format_date(need_date_list[i]), "(", i + 1, "/", len(need_date_list), ")")
+            curr_date = need_date_list[i]
+
+            # 计算请求参数并执行请求
+            curr_date_timestamp = str((datetime.strptime(curr_date, "%Y%m%d") - datetime(1970, 1, 1)).total_seconds())
+            self._DATE_LIST_DATA["time"] = curr_date_timestamp
+            response = tool.do_request(
+                self._DATE_LIST_URL, method="post",
+                headers=self._DATE_LIST_HEADERS, data=self._DATE_LIST_DATA
+            )
+
+            # 解析请求的返回结果
             if response.status_code == 200:
                 response_json = json.loads(response.content.decode())
-                # print(response_json)
-                for curr_date_str, date_info in response_json["data"]["scheduleList"].items():
-                    if int(end_date) >= int(curr_date_str) >= int(start_date):
-                        if date_info["list"]:
-                            for match in date_info["list"]:
-                                result.append({
-                                    "date": curr_date_str,
-                                    "race_id": match["scheduleid"],
-                                    "team_a_name": match["oneseedname"],
-                                    "team_b_name": match["twoseedname"],
-                                    "start_time": match["starttime"],
-                                    "team_a_score": match["onewin"],
-                                    "team_b_score": match["twowin"],
-                                    "contest_name": match["ename"],
-                                    "match_name": match["groupname"],
-                                    "team_a_score_per": match["oneScore"],
-                                    "team_b_score_per": match["twoScore"],
-                                })
+                for curr_date, date_info in response_json["data"]["scheduleList"].items():
+                    if int(start_date.strftime("%Y%m%d")) <= int(curr_date) <= int(end_date.strftime("%Y%m%d")):
+                        for match in date_info["list"]:
+                            print(self.format_date(curr_date), match)
+                            result.append({
+                                "schedule_id": match["scheduleid"],  # 比赛ID(一场完整的BO1/BO3/BO5的比赛称为比赛)
+                                "date": self.format_date(curr_date),  # 比赛日期
+                                "time": match["starttime"],  # 比赛时间:比赛开始时间
+                                "event_id": match["eid"],  # 赛事ID
+                                "event_name": match["ename"],  # 赛事名称
+                                "event_group_name": match["groupname"],  # 赛事赛段
+                                "stageid": match["stageid"],  # 疑似赛事ID
+                                "bo_num": match["bonum"],  # 比赛场次:BO1=1,BO3=3,BO5=5
+                                "team_a_id": match["oneseedid"],  # 队伍A的ID
+                                "team_a_name": match["oneseedname"],  # 队伍A的名称
+                                "team_b_id": match["twoseedid"],  # 队伍B的ID
+                                "team_b_name": match["twoseedname"],  # 队伍B的名称
+                                "team_a_win": match["onewin"],  # 队伍A的获胜小场数
+                                "team_b_win": match["twowin"],  # 队伍B的获胜小场数
+                                "team_a_score": str(match["oneScore"]),  # 队伍A的每小场得分
+                                "team_b_score": str(match["twoScore"]),  # 队伍B的每小场得分
+                            })
             time.sleep(5)
 
         return result
+
+    @staticmethod
+    def format_date(string):
+        return string[0:4] + "-" + string[4:6] + "-" + string[6:8]
 
 
 # ------------------- 单元测试 -------------------
 if __name__ == "__main__":
     print(SpiderWanplusLolDateList().running(
-        start_date=datetime.datetime.today() + datetime.timedelta(days=-7),  # 抓取开始日期
-        end_date=(datetime.datetime.today() + datetime.timedelta(days=7))  # 抓取结束日期
+        start_date=datetime.today() + timedelta(days=-2),  # 抓取开始日期
+        end_date=datetime.today() + timedelta(days=-1)  # 抓取结束日期
     ))
